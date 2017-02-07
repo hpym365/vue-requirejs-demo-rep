fd541ca81a1beb06101476f3dbbcd0c6d1d720f5
hpym365 <hpym365@gmail.com>
2017-02-07 14:57:44 +0800

    2017-2-3 14:22:47

--- src/test/js/modules/grid.js
+++ src/test/js/modules/grid.js
@@ -14,33 +14,33 @@ define(['jquery', 'components'], function ($, com) {
     var optionsdata = {
       fields: [{name: '名字', col: 'name'}, {name: '年龄', col: 'age'}],
       datalist: [{name: '张三', age: '22'}, {name: '李四', age: '21'}],
-      test: function (ziduan, shuju) {
-        if (ziduan.col === 'name') {
-          if (shuju[ziduan.col] === '张三') {
-            return '<h1>这是张三啊</h1>'
-          } else {
-            //  var div = $('<button @click="options.clickevent">' + shuju[ziduan.col] + '</button>')
-            return '<button>' + shuju[ziduan.col] + this + '</button>'
-          }
-        }
-        return null
-      },
+      // test: function (ziduan, shuju) {
+      //   if (ziduan.col === 'name') {
+      //     if (shuju[ziduan.col] === '张三') {
+      //       return '<h1>这是张三啊</h1>'
+      //     } else {
+      //       //  var div = $('<button @click="options.clickevent">' + shuju[ziduan.col] + '</button>')
+      //       return '<button>' + shuju[ziduan.col] + this + '</button>'
+      //     }
+      //   }
+      //   return null
+      // },
       clickevent: function () {
         window.alert('张三的点击事件')
       },
-      clickevent1: function () {
+      axcvcv: function () {
         window.alert('其他人的点击事件')
       },
       domfun: function (el, fields, datalist) {
         debugger
         if (fields.col === 'name') {
           if (datalist[fields.col] === '张三') {
-            el.innerHTML = '<h1>张三啊</h1>'
+            el.innerHTML = '<h1>张三啊'+datalist.age+'</h1>'
             el.addEventListener('click', this.clickevent)
           }
           else {
-            el.innerHTML = '<button>' + datalist[fields.col] + '</button>'
-            el.addEventListener('click', this.clickevent1)
+            el.innerHTML = '<button>' + datalist[fields.col] + '</button><button>' + datalist[fields.col] + '</button>'
+            el.addEventListener('click', this.axcvcv)
           }
         }
         return el
